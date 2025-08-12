a0e5bfa943de9aa358a48a411471a3a8805b7242
hooss-only <hoossonly@gmail.com>
2025-08-12 19:40:08 +0900

    Add printing string

--- drivers/screen.h
+++ drivers/screen.h
@@ -5,6 +5,8 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 200
 
+#define WHITE 15
+
 int put_pixel_at(int x, int y, unsigned char color);
 void put_char_at(int x, int y, unsigned char color, char c);
 void put_string_at(int x, int y, unsigned char color, char* str);
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,5 +3,5 @@
 void kernel_main() {
   clear_screen(0);
 
-  put_string_at(10, 10, 1, "ABC CBCB AAAA");
+  put_string_at(10, 10, WHITE, "MINER OS");
 }
