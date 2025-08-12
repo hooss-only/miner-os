3974e745749c316986eff91501713a61ebe6cad4
hooss-only <hoossonly@gmail.com>
2025-08-12 21:58:54 +0900

    Add arrow contorl in menu

--- game/menu.c
+++ game/menu.c
@@ -1,22 +1,51 @@
 #include "menu.h"
 
+#include "../cpu/ports.h"
+#include "../cpu/irq.h"
 #include "../drivers/screen.h"
 
+int sel = 0;
+
 void draw_menu();
+
+#define ARROW_UP 0x48
+#define ARROW_DOWN 0x50
+static void keyboard_handle(registers_t regs) {
+  unsigned char scancode = port_byte_in(0x60);
+
+  if (scancode == ARROW_UP) sel--;
+  else if (scancode == ARROW_DOWN) sel++;
+
+  if (sel < 0) sel = 0;
+  if (sel > 2) sel = 2;
+
+  draw_menu();
+}
 
 /* PUBLIC FUNCTIONS */
 void init_menu() {
   draw_menu();
+  
+  register_interrupt_handler(IRQ1, keyboard_handle);
 }
 
 /* PRIVATE FUNCTIONS */
+
+const char* MENU[] = {
+  "EASY",
+  "NORMAL",
+  "HARD"
+};
+
 void draw_menu() {
   clear_screen(0);
   put_string_at(10, 10, WHITE, "MINER OS");
   put_string_at(10, 20, LIGHT_GRAY, "A 32BITS OPERATING SYSTEM");
   put_string_at(10, 30, LIGHT_GRAY, "ONLY FOR MINESWEEPER GAME");
 
-  put_string_at(15, 45, LIGHT_YELLOW, "EASY   10 X 10");
-  put_string_at(15, 55, WHITE, "NORMAL 15 X 15");
-  put_string_at(15, 65, WHITE, "HARD   20 X 20");
+  for (int i=0; i<3; i++) {
+    char color = WHITE;
+    if (sel == i) color = LIGHT_YELLOW;
+    put_string_at(15, 45+i*10, color, MENU[i]);
+  }
 }
